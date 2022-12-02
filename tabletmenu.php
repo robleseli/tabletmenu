e7cb67efea3bc95685fe3aa298d2f467ae5b42fa
Eli Robles <97868399+robleseli@users.noreply.github.com>
2022-12-02 11:58:09 -0600

    Add files via upload

---
+++ tabletmenu.php
@@ -0,0 +1,92 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Main Menu</title>
+<style type="text/css">
+
+<!--
+.Arial {
+	font-family: Arial, Helvetica, sans-serif;
+	color: navy;
+    font-size: 50px
+}
+  a:link, a:visited {
+  font-family: Arial, Helvetica, sans-serif;
+  background-color: navy;
+  color: white;
+  padding: 30px 65px;
+  text-align: center;
+  text-decoration: none;
+  display: block;
+}
+a:hover, a:active {
+  background-color: navy;
+}
+
+div.fixed {
+  position: fixed;
+  width: 100%;
+  bottom: 10px;
+} 
+    
+.a {
+  background-color: navy; 
+  color: white;
+  padding: 25px 10px;
+  text-align: center;
+  text-decoration: bold;
+  display: block;
+  font-size: 16px;
+  cursor: pointer;
+}
+
+-->
+</style>
+    
+<link rel="shortcut icon" type="image/ico" href="favicon.ico" />
+</head>
+
+<body>
+    <p>&nbsp;</p>
+    
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+            <h1 align="center" class="Arial" color="blue">Main Menu</h1>
+
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+
+
+    <tr>
+        <th align="center"><a href="https://app.smartsheet.com/b/form/8c320491961d4aa087963baacd3710bf">SUPERVISORS</a></th>
+        
+    </tr>
+    
+    
+    <p>&nbsp;</p>
+    
+    
+    <tr>
+        <th align="center"><a href="https://app.smartsheet.com/b/form/b4ac480328ab47758e16f6a6539916a8">VISITORS</a></th>
+    </tr>
+   
+  <p>&nbsp;</p>
+
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+
+    <p>&nbsp;</p>
+    <p>&nbsp;</p>
+
+    <table style="width: 100%">
+        <tbody><tr><td><img src="penskesmall.gif" border="0"></td></tr>
+        <tr><td width="100%" height="20px" bgcolor="#FDD017"></td></tr>
+        <tr><td width="100%" height="20px" bgcolor="#151B8D"></td></tr>
+        </tbody>
+    </table>
+
+</body>
+</html>
